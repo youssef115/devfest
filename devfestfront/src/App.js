9556f543fd667fc336ba11d29d98f7366575bd6a
AlaHarroum <harroumala20@gmail.com>
2023-12-17 05:39:09 +0100

    test tes

--- devfestfront/src/App.js
+++ devfestfront/src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Layout from './pages/Layout';
 import Home from './pages/Home';
+import Login from './pages/Login';
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route path="home" element={<Home/>}/>
+          <Route path="login" element={<Login/>}/>
         </Route>
       </Routes>
     </>
--- devfestfront/src/pages/Login.jsx
+++ devfestfront/src/pages/Login.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import { Container, Row, Col, Form, FormGroup, Button } from 'reactstrap'
+import { Link, json, useNavigate } from "react-router-dom";
+import font from "../assets/font_login.png"
+import "../styles/login.css"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <section>
+      <Container>
+        <Row>
+          <Col lg="11" className="m-auto">
+            <div className="login__container d-flex justify-content-between">
+              <div className="login__form">
+                <h2>Login</h2>
+
+                <Form >
+                  <FormGroup>
+                    <input
+                      type="email"
+                      placeholder="Email"
+                      required
+                      id="email"
+                      
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <input
+                     
+                      type="password"
+                      placeholder="Password"
+                      required
+                      id="password"
+
+                    />
+                  </FormGroup>
+                  <Button  className="btn  auth__btn" type="Button">
+                    Sign In
+                  </Button>
+                </Form>
+                <p>
+                If you don't have an account, <Link to="/register">Sign Up</Link>
+                </p>
+              </div>
+              <div className="login__img">
+                <img src={font} alt="" />
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+    </>
   )
 }
 
