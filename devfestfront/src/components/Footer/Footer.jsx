dda720f2f62ccc7a3d926eb0e3f5eb862b994042
AlaHarroum <harroumala20@gmail.com>
2023-12-17 04:55:46 +0100

    Footer there

--- devfestfront/src/components/Footer/Footer.jsx
+++ devfestfront/src/components/Footer/Footer.jsx
@@ -1,9 +1,124 @@
 import React from 'react'
+import './footer.css'
 
-function Footer() {
-  return (
-    <div>Footer</div>
-  )
+import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap'
+import { Link } from 'react-router-dom'
+import logo from "../../assets/R.png"
+import logo1 from "../../assets/R.png"
+
+const quick__links = [
+    {
+        path: '/home',
+        display: 'Home'
+    },
+    {
+        path: '/news',
+        display: 'News'
+    },
+]
+
+const quick__links2 = [
+    {
+        path: '/login',
+        display: 'Login'
+    },
+    {
+        path: '/register',
+        display: 'Register'
+    },
+]
+
+const Footer = () => {
+
+    const year = new Date().getFullYear()
+
+    return (
+        <footer className="footer">
+            <Container>
+                <Row>
+                    <Col lg='3'>
+                        <div className="logo">
+                            <img src={logo1} alt="" />
+                            <p>La solidarité unit les personnes par le partage et le soutien, tandis que la paix crée un environnement sans conflit, favorisant le respect mutuel et la coopération.</p>
+
+                            <div className="social__links d-flex align-items-center gap-4">
+                                <span>
+                                    <Link to='#'><i class="ri-youtube-line"></i></Link>
+                                </span>
+                                <span>
+                                    <Link to='#'><i class="ri-github-fill"></i></Link>
+                                </span>
+                                <span>
+                                    <Link to='#'><i class="ri-facebook-circle-line"></i></Link>
+                                </span>
+                                <span>
+                                    <Link to='#'><i class="ri-instagram-line"></i></Link>
+                                </span>
+                            </div>
+                        </div>
+                    </Col>
+
+                    <Col lg='3'>
+                        <h5 className="footer__link-title">Discover</h5>
+                        <ListGroup className="footer__quick-links">
+                            {
+                                quick__links.map((item, index) => (
+                                    <ListGroupItem key={index} className='ps-0 border-0'>
+                                        <Link to={item.path}>{item.display}</Link>
+                                    </ListGroupItem>
+                                ))
+                            }
+                        </ListGroup>
+                    </Col>
+                    <Col lg='3'>
+                        <h5 className="footer__link-title">Quick Links</h5>
+                        <ListGroup className="footer__quick-links">
+                            {
+                                quick__links2.map((item, index) => (
+                                    <ListGroupItem key={index} className='ps-0 border-0'>
+                                        <Link to={item.path}>{item.display}</Link>
+                                    </ListGroupItem>
+                                ))
+                            }
+                        </ListGroup>
+                    </Col>
+                    <Col lg='3'>
+                        <h5 className="footer__link-title">Contact</h5>
+                        <ListGroup className="footer__quick-links">
+                            <ListGroupItem className='ps-0 border-0 d-flex align-items-center gap-3'>
+                                <h6 className='mb-0 d-flex aligh-items-center gap-2'>
+                                    <span><i class="ri-map-pin-line"></i></span>
+                                    Address:
+                                </h6>
+
+                                <p className='mb-0'>Sousse, Tunisia</p>
+                            </ListGroupItem>
+                            <ListGroupItem className='ps-0 border-0 d-flex align-items-center gap-3'>
+                                <h6 className='mb-0 d-flex aligh-items-center gap-2'>
+                                    <span><i class="ri-mail-line"></i></span>
+                                    Email:
+                                </h6>
+
+                                <p className='mb-0'>exemple@issatso.u-sousse.tn</p>
+                            </ListGroupItem>
+                            <ListGroupItem className='ps-0 border-0 d-flex align-items-center gap-3'>
+                                <h6 className='mb-0 d-flex aligh-items-center gap-2'>
+                                    <span><i class="ri-phone-fill"></i></span>
+                                    Phone:
+                                </h6>
+
+                                <p className='mb-0'>+0123456789</p>
+                            </ListGroupItem>
+
+                        </ListGroup>
+                    </Col>
+                    <Col lg='12' className='text-center pt-5'>
+                        <p className="copyright">Copyright {year}, design and develop by Bug-Finders Team. All rights resreved.</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
 }
 
 export default Footer
--- devfestfront/src/pages/Home.jsx
+++ devfestfront/src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Container, Row, Col } from "reactstrap"
 
+import "../styles/home.css"
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div className="heroSection">
+      <div className="Shadow">
+        <p className="hero__text animate-bounce">
+          Nomber of Donators : 1.000.000
+        </p>
+        <button className="home__btn">Explorer détails</button>
+      </div>
+    </div>
+
+
+    </>
   )
 }
 
